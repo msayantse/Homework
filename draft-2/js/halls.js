1bae3105fe01770258cfcb108b993a58d1a6e09b
msayantse <msayantse@gmail.com>
2026-08-19 23:12:12 +0900

    Add files via upload

--- draft-2/js/halls.js
+++ draft-2/js/halls.js
@@ -1,4 +1,4 @@
-export const sizes = () => {
+export const halls = () => {
   const hallsList = document.querySelector('[data-halls="list"]');
   const hallsButtons = document.querySelectorAll('[data-halls="button"]');
 
