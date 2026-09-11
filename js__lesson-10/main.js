964969bc1a1cfa69d930ff8d51cdcef5e7d3f1d1
msayantse <msayantse@gmail.com>
2026-09-11 18:41:03 +0900

    Add files via upload

---
+++ js__lesson-10/main.js
@@ -0,0 +1,27 @@
+// Задача 1
+
+const firstName = "Иван";
+const lastName = "Иванов";
+const student = true;
+
+//Задача 2
+const age = 20;
+const currentYear = 2026;
+const birthYear = currentYear - age;
+
+//Задача 3
+
+console.log(
+  `Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${student}`,
+);
+
+//Задача 4
+
+let a = "123";
+let b = +"456";
+let c = Number("789");
+let d = Boolean(0);
+let e = Boolean(" ");
+let result = a + b + c + d + e;
+
+//let result = 123456789falsetrue;
